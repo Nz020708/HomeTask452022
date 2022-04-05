769e17cdaf18aa134731a38e9e84b22c67888cf0
NazMzadeh <100687473+Nz020708@users.noreply.github.com>
2022-04-05 11:47:31 +0400

    Add files via upload

---
+++ HomeTask4522/ExtensionMethods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask4522
+{
+    public static class ExtensionMethods
+    {
+        
+       
+        public static bool CustomContains(this string word,string Word)
+        {
+            bool result ;
+            if (Word.Contains(word))
+            {
+                Console.WriteLine("Daxildir.");
+                result = true;
+                Console.WriteLine(result);
+            }
+
+            else
+            {
+                Console.WriteLine("Daxil deyil");
+                result = false;
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+        public static bool CustomContains( this char letter, string Word)
+        {
+            bool result;
+
+            if (Word.Contains(letter))
+            {
+                Console.WriteLine("Daxildir.");
+                result = true;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Daxil deyil.");
+                result = false;
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+
+        public static void IsPrime(this int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine($"{number} murekkeb ededdir");
+                    break;
+
+
+                }
+                else
+                {
+                    if (i == number - 1)
+                    {
+                        Console.WriteLine($"{number} sade ededdir");
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
---
+++ HomeTask4522/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeTask4522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char l = 'p';
+            ExtensionMethods.CustomContains('p', "apple");
+            string w = "Apple";
+            ExtensionMethods.CustomContains("na", "apple");
+            ExtensionMethods.IsPrime(19);
+        }
+    }
+}
